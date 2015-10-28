fd566eb7aae38ebfd5ef883e97fab3a8dc2889a6
Can Erdogan <canerdogan.89@gmail.com>
2015-10-27 22:25:29 -0400

    Visualized the bump function. Next: Check if convolution works for collision checking

---
+++ bump.m
@@ -0,0 +1,10 @@
+xs = -2:0.01:2;
+ys01 = exp(1-power(abs(xs),-0.1)).^-1;
+ys1 = exp(1-power(abs(xs),-1)).^-1;
+ys10 = exp(1-power(abs(xs),-10)).^-1;
+ys100 = exp(1-power(abs(xs),-100)).^-1;
+plot(xs,ys01,'r',xs,ys1,'g',xs,ys10,'b',xs,ys100,'k');
+legend('a=0.1','a=1.0','a=10.0','a=100.0')
+axis([-2 2 0 10])
+title('Bump function with different \alpha values', 'FontSize', 14);
+ylabel('\psi_{\alpha}(x)');
