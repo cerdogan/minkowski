0c4f1c076c7ddd81ccbdf8ab23314d51bed62f51
Can Erdogan <canerdogan.89@gmail.com>
2015-11-30 22:16:17 -0500

    next: nonuniform samples

--- s00_bump.m
+++ s00_bump.m
@@ -6,7 +6,6 @@ function v = s00_bump (x, alpha, offset_)
   else offset = 0; end;
   v = [];
   for i = 1 : numel(x)
-    abs(x(i))
     if(abs(x(i)) < (1-1e-5)), v(end+1) = exp(1-power(abs(x(i))+offset,-alpha)).^-1;
     else v(end+1) = 0; end;
   end
--- s08_uniformCentersB0.m
+++ s08_uniformCentersB0.m
@@ -5,29 +5,56 @@
 % of the ball function. For implementation, assume center points are on
 % a regular grid for now. 
 
-%function [] = s08_uniformCentersB0 ()
+function [] = s08_uniformCentersB0 ()
 
   % Draw the possible circle centers
   clf;
-  d = 0.2; low = 0; high = 5;
+  d = 0.2; low = -2; high = 3;
   K = (high - low) / d + 1
   ps = [];
   for x = low : d : high, for y = low : d : high, ps(end+1,:) = [x,y]; end, end;
   plot(ps(:,1),ps(:,2),'.'); hold on;
   
   % Draw the three circles
-  p1 = [1 + d*2, 3]; p2 = [3 - d*2, 4]; p3 = [3+d*2, 1];
+  p1 = [-1 + d*2, 1]; p2 = [1 - d*2, 2]; p3 = [1+d*2, -1];
   drawCircle(p1, 0.5, 'r');
   drawCircle(p2, 0.5, 'g');
   drawCircle(p3, 0.5, 'b');
-  drawCircle([1,1], 0.5, 'c');
+  drawCircle([0,0], 0.5, 'c');
   
   % Generate the vector for centers
   rho = zeros(K,K);
-  rho(K-round(p1(2)/d),round(p1(1)/d)+1) = 1;
-  rho(K-round(p2(2)/d),round(p2(1)/d)+1) = 1;
-  rho(K-round(p3(2)/d),round(p3(1)/d)+1) = 1;
-
+  rho(K-round((p1(2)-low)/d),round((p1(1)-low)/d)+1) = 1;
+  rho(K-round((p2(2)-low)/d),round((p2(1)-low)/d)+1) = 1;
+  rho(K-round((p3(2)-low)/d),round((p3(1)-low)/d)+1) = 1;
+  
   % Generate the vector for a circle centered at 1,1
-%end
+  tic
+  v0 = s00_bump(sqrt(sum(ps.*ps,2))/0.5, 1);
+  %v0(find(isinf(v0))) = 1e1;
+  v0 = reshape(v0, K, K);
+
+  % Compute the fourier transforms 
+  rhof = fft2(rho);
+  v0f = fft2(v0);
+  rhov0f = rhof.*v0f;
+  toc
+  
+  % Generate the descriptor function for the entire shape
+  tic
+  psp1 = ps - repmat(p1, size(ps,1), 1);
+  psp2 = ps - repmat(p2, size(ps,1), 1);
+  psp3 = ps - repmat(p3, size(ps,1), 1);
+  v = s00_bump(sqrt(sum(psp1.*psp1,2))/0.5, 1) + ...
+    s00_bump(sqrt(sum(psp2.*psp2,2))/0.5, 1) + ...
+    s00_bump(sqrt(sum(psp3.*psp3,2))/0.5, 1);
+  %v(find(isinf(v))) = 1e1;
+  v = reshape(v,26,26);
+  
+  % Compare with the fourier transform of the entire shape
+  vf = fft2(v);
+  toc
+  assert(max(max(max(real(vf)-real(rhov0f))), max(max(imag(vf)-imag(rhov0f)))) < 1e-5);
+
+end
 
